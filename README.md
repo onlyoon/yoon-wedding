2b5e3a3d724b9fa51c6df3fe7d9ca92b90d57334
SANGYOON PARK <parkyoon97@gmail.com>
2025-06-22 01:35:11 +0900

    wedding invitation

---
+++ README.md
@@ -0,0 +1,5 @@
+**청첩장**
+
+> https://sang-yoon.github.io/
+
+<img src="https://github.com/user-attachments/assets/66bd21df-d0dd-49c7-ae9b-e5e4a801101b" style="width:400px" />
---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const account = [
+  {
+    name: "박상윤",
+    account: "신한은행 110-492-084633",
+    group: "박상윤",
+  },
+  {
+    name: "박진현",
+    account: "하나은행 XXX-XXXXXX-XXXXX",
+    group: "박상윤",
+  },
+  {
+    name: "하수경",
+    account: "우리은행 XXX-XXXXXX-XX-XXX",
+    group: "박상윤",
+  },
+];
