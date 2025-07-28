026b1d0ca8ab2a985e81fe8128a06da11c986dae
SANGYOON PARK <parkyoon97@gmail.com>
2025-07-28 23:58:29 +0900

    refactor: delete map, add role, background fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,18 @@
 export const account = [
   {
+    role: "신랑",
     name: "박상윤",
     account: "신한은행 110-492-084633",
     group: "박상윤",
   },
   {
+    role: "신랑 부",
     name: "박진현",
     account: "하나은행 30918-01627-5",
     group: "박상윤",
   },
   {
+    role: "신랑 모",
     name: "하수경",
     account: "국민은행 20321-16675-45",
     group: "박상윤",
